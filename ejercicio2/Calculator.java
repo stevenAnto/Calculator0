ad214e64e273dba45c33de43697e61969e1993f1
HenryGalvezQ <hgalvezq@unsa.edu.pe>
2022-04-28 09:00:02 -0500

    funcion resta agregada

--- ejercicio2/Calculator.java
+++ ejercicio2/Calculator.java
@@ -2,10 +2,12 @@ import java.util.*;
 
 public class Calculator{
 
-  public int add(int a, int b){//Esteven
+  public int add(int a, int b){ //Esteven
     return a+b; 
   }
-  public int sub(int a, int b){ return 0; }
+  public int sub(int a, int b){ //Henry
+    return a-b;
+  }
   public int mul(int a, int b){ return 0; }
   public int div(int a, int b){ return 0; }
   public int mod(int a, int b){ return 0; }
