f676945ccc74fe06ad6ae6b67aa1a5572bf17882
STV <ecalcinap@unsa.edu.pe>
2022-04-28 08:55:37 -0500

    se hizo el metodo add

--- ejercicio2/Calculator.java
+++ ejercicio2/Calculator.java
@@ -1,7 +1,10 @@
 import java.util.*;
 
 public class Calculator{
-  public int add(int a, int b){ return 0; }
+
+  public int add(int a, int b){//Esteven
+    return a+b; 
+  }
   public int sub(int a, int b){ return 0; }
   public int mul(int a, int b){ return 0; }
   public int div(int a, int b){ return 0; }
