5768bb86e6191c96159f210e9369cfd6a92a3da6
atn25042002 <aacot@unsa.edu.pe>
2022-04-28 09:06:27 -0500

    Agregando función multiplicación

--- ejercicio2/Calculator.java
+++ ejercicio2/Calculator.java
@@ -8,7 +8,9 @@ public class Calculator{
   public int sub(int a, int b){ //Henry
     return a-b;
   }
-  public int mul(int a, int b){ return 0; }
+  public int mul(int a, int b){ //Anthony
+    return a*b; 
+  }
   public int div(int a, int b){ return 0; }
   public int mod(int a, int b){ return 0; }
   //Hola
